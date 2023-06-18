c5243e4b2b6a97bbf2fbb4dc9345b294f0581529
rb-au <raj@aureusanalytics.com>
2023-06-18 12:19:48 +0530

    Add container

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,37 +11,52 @@ import { usePathname } from "next/navigation";
 import Button from "@mui/material/Button";
 import Image from 'next/image';
 import styles from './../app/page.module.css';
+import { Container } from "@mui/material";
 
 export default function Header() {
     const pathname = usePathname();
     const isHome = pathname === "/" || pathname === undefined;
     return (
         <Box sx={{ flexGrow: 1, padding: 6 }}>
-            <AppBar position="static" sx={{ background: "transparent" }}>
-                <Toolbar>
-                    <Box component="div" sx={{ flexGrow: 0 }}>
-                        <Link href="/" sx={{}}>
-                            <Typography
-                                variant="h4"
-                                component="div"
-                                sx={{
-                                    flexGrow: 1,
-                                    fontFamily: "Michroma",
-                                    cursor: "pointer",
-                                    marginRight: 2,
-                                }}
-                            >
-                                Notary+
-                            </Typography>
-                        </Link>
-                    </Box>
-                    <Box
-                        sx={{
-                            flexGrow: 1,
-                            display: { xs: "none", md: "flex", marginLeft: 2 },
-                        }}
-                    >
-                        <a href="/#protocol-info">
+            <Container maxWidth="xl">
+
+                <AppBar position="static" sx={{ background: "transparent" }}>
+                    <Toolbar>
+                        <Box component="div" sx={{ flexGrow: 0 }}>
+                            <Link href="/" sx={{}}>
+                                <Typography
+                                    variant="h4"
+                                    component="div"
+                                    sx={{
+                                        flexGrow: 1,
+                                        fontFamily: "Michroma",
+                                        cursor: "pointer",
+                                        marginRight: 2,
+                                    }}
+                                >
+                                    Notary+
+                                </Typography>
+                            </Link>
+                        </Box>
+                        <Box
+                            sx={{
+                                flexGrow: 1,
+                                display: { xs: "none", md: "flex", marginLeft: 2 },
+                            }}
+                        >
+                            <a href="/#protocol-info">
+                                <Button
+                                    sx={{
+                                        my: 2,
+                                        color: "white",
+                                        display: "block",
+                                        marginLeft: 2,
+                                        marginRight: 2,
+                                    }}
+                                >
+                                    PROTOCOL
+                                </Button>
+                            </a>
                             <Button
                                 sx={{
                                     my: 2,
@@ -51,54 +66,43 @@ export default function Header() {
                                     marginRight: 2,
                                 }}
                             >
-                                PROTOCOL
+                                USE CASES
                             </Button>
-                        </a>
-                        <Button
-                            sx={{
-                                my: 2,
-                                color: "white",
-                                display: "block",
-                                marginLeft: 2,
-                                marginRight: 2,
-                            }}
-                        >
-                            USE CASES
-                        </Button>
-                        <Button
-                            sx={{
-                                my: 2,
-                                color: "white",
-                                display: "block",
-                                marginLeft: 2,
-                                marginRight: 2,
-                            }}
-                        >
-                            FAQ
-                        </Button>
-                    </Box>
-
-                    <Stack direction="row" justifyContent='center' alignItems="center">
-                        <div>
-                            <a
-                                href="https://fvm.filecoin.io/"
-                                target="_blank"
-                                rel="noopener noreferrer"
+                            <Button
+                                sx={{
+                                    my: 2,
+                                    color: "white",
+                                    display: "block",
+                                    marginLeft: 2,
+                                    marginRight: 2,
+                                }}
                             >
-                                <Image
-                                    src="/fvm.svg"
-                                    alt="FVM Logo"
-                                    width={80}
-                                    height={80}
-                                    priority
-                                    className={styles.vercelLogo}
-                                />
-                            </a>
-                        </div>
-                        <Connect />
-                    </Stack>
-                </Toolbar>
-            </AppBar>
+                                FAQ
+                            </Button>
+                        </Box>
+
+                        <Stack direction="row" justifyContent='center' alignItems="center">
+                            <div>
+                                <a
+                                    href="https://fvm.filecoin.io/"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <Image
+                                        src="/fvm.svg"
+                                        alt="FVM Logo"
+                                        width={80}
+                                        height={80}
+                                        priority
+                                        className={styles.vercelLogo}
+                                    />
+                                </a>
+                            </div>
+                            <Connect />
+                        </Stack>
+                    </Toolbar>
+                </AppBar>
+            </Container>
         </Box>
     );
 }
