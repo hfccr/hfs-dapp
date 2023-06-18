56d2be6522764ba2cfb1ff1c1307b11b337b36d9
rb-au <raj@aureusanalytics.com>
2023-06-18 15:14:06 +0530

    Add constants

---
+++ src/constants/contracts.js
@@ -0,0 +1,8 @@
+const Contracts = {
+    NotaryGovernanceToken: '0x355Fef49D3a5cfc3dcF464Ed5409d415e6eAED27',
+    DataCapToken: '0xfEc1322C37e15f3379C29e28830041f3Bb6E5f06',
+    NotaryGovernor: '0x0aB05764810ADa42BeFa9E6438d5444FEa76825b',
+    Clients: '0xA5D0111bB00B17d6CBa37fD3D08F1657a1C9776B'
+};
+
+export default Contracts;
